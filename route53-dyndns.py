4e5ea0817230e80a35d41d3bf1ed6d16e49c2ae6
Juan Orti Alcaine <jortialc@redhat.com>
2023-09-12 18:31:15 +0200

    Move some code to functions

--- route53-dyndns.py
+++ route53-dyndns.py
@@ -24,7 +24,6 @@ import boto3
 from ipaddress import IPv4Address, IPv6Address, IPv6Network, ip_address, ip_network
 import netifaces
 from subprocess import check_output, SubprocessError
-from time import sleep
 from signal import signal, SIGINT, SIGTERM
 import logging
 from typing import Optional
@@ -41,8 +40,8 @@ def get_ipv4_address(source: dict) -> Optional[IPv4Address]:
             with open(source["file"], "r") as f:
                 content = f.readline().rstrip()
             return ip_address(content)
-        except OSError as e:
-            print(f"Error reading IPv4 address from file {source['file']}: {e}")
+        except OSError as exception:
+            print(f"Error reading IPv4 address from file {source['file']}: {exception}")
     elif source["type"] == "url":
         curl_cmd = [shutil.which('curl'), '--ipv4',
                     '--silent', '--max-time', '10', source["url"]]
@@ -50,10 +49,10 @@ def get_ipv4_address(source: dict) -> Optional[IPv4Address]:
             logging.debug(f"Running command {curl_cmd}")
             output = check_output(curl_cmd)
             return ip_address(output.decode('utf-8').strip())
-        except SubprocessError as e:
+        except SubprocessError as exception:
             print("Error getting IPv4 address using curl:")
             print(curl_cmd)
-            print(e)
+            print(exception)
     else:
         print("Unknown IPv4 source type'{}'".format(source["type"]))
         raise ValueError
@@ -79,8 +78,8 @@ def get_ipv6_prefix(source: dict) -> Optional[IPv6Network]:
             with open(source["file"], "r") as f:
                 content = f.readline().rstrip()
             return ip_network(content)
-        except OSError as e:
-            print(f"Error reading IPv6 prefix from file {source['file']}: {e}")
+        except OSError as exception:
+            print(f"Error reading IPv6 prefix from file {source['file']}: {exception}")
     elif source["type"] == "url":
         if "prefixlen" not in source.keys():
             print("You need to specify 'prefixlen' for the IPv6 URL method")
@@ -91,10 +90,10 @@ def get_ipv6_prefix(source: dict) -> Optional[IPv6Network]:
             logging.debug(f"Running command {curl_cmd}")
             output = check_output(curl_cmd).decode('utf-8')
             return ip_network(output.strip() + '/' + str(source["prefixlen"]), strict=False)
-        except SubprocessError as e:
+        except SubprocessError as exception:
             print("Error getting IPv6 prefix using curl:")
             print(curl_cmd)
-            print(e)
+            print(exception)
     else:
         print("Unknown IPv6 source type '{}'".format(source["type"]))
         raise ValueError
@@ -126,46 +125,47 @@ def finish(_signo, _stack_frame):
     sys.exit(0)
 
 
-def get_base_domain(fqdn):
+def get_base_domain(fqdn: str) -> str:
     return '.'.join(fqdn.split('.')[-2:])
 
 
-def needs_update(desired_record, current_records):
+def needs_update(desired: dict, current: list) -> (bool, bool):
     """Checks if a desired record exists in the list of retrieved records and if needs to be updated.
 
     Returns a tuple of booleans: (exists, updated)"""
     exists = False
     updated = False
-    for record in current_records:
+    for record in current:
         decoded_name = record['Name'].encode('utf-8').decode('unicode_escape')
-        if decoded_name == desired_record['name'] and \
-                record['Type'] == desired_record['type']:
+        if decoded_name == desired['Name'] and \
+                record['Type'] == desired['Type']:
             exists = True
-            if record['TTL'] == desired_record['ttl'] and \
-                    record['ResourceRecords'][0]['Value'] == desired_record['value']:
+            if record['TTL'] == desired['TTL'] and \
+                    record['ResourceRecords'][0]['Value'] == desired['ResourceRecords'][0]['Value']:
                 updated = True
             break
     return exists, updated
 
 
-def create_route53_change(action, record):
+def create_route53_change(action: str, record: dict) -> dict:
     return {'Action': action,
             'ResourceRecordSet': {
-                'Name': record['name'],
-                'Type': record['type'],
-                'TTL': record['ttl'],
+                'Name': record['Name'],
+                'Type': record['Type'],
+                'TTL': record['TTL'],
                 'ResourceRecords': [
-                    {'Value': record['value']}
+                    {'Value': record['ResourceRecords'][0]['Value']}
                 ]
             }}
 
 
-def create_route53_change_batch(changes):
+def create_route53_change_batch(changed_records: list) -> dict:
+    """Create a Route 53 change batch from a list of changed records"""
     return {'Comment': "Change created by route53-dyndns.py",
-            'Changes': changes}
+            'Changes': changed_records}
 
 
-def get_route53_domain_records(domain_id, **kwargs):
+def get_route53_domain_records(domain_id: str, **kwargs) -> list:
     if 'StartRecordName' in kwargs and 'StartRecordType' in kwargs and 'StartRecordIdentifier' not in kwargs:
         resource_record_sets = route53.list_resource_record_sets(HostedZoneId=domain_id,
                                                                  StartRecordName=kwargs['StartRecordName'],
@@ -187,11 +187,127 @@ def get_route53_domain_records(domain_id, **kwargs):
     return records
 
 
-def find_record_in_list(record_list, wanted_name, wanted_type):
+def find_record_in_list(record_list: list, wanted_name: str, wanted_type: str) -> Optional[dict]:
     """Find a specific record in a list of records"""
     for record in record_list:
         if record['Name'] == wanted_name and record['Type'] == wanted_type:
             return record
+
+
+def get_all_base_domains() -> dict:
+    """Get all the base domains and their Route 53 IDs from the list of configured records"""
+    all_domains = []
+    domain_ids = {}
+    for record in conf['dns_records']:
+        domain = get_base_domain(record['hostname'])
+        if domain not in all_domains:
+            all_domains.append(domain)
+    for domain in all_domains:
+        hosted_zones = route53.list_hosted_zones_by_name(DNSName=domain)
+        domain_ids[domain] = {'Id': hosted_zones['HostedZones'][0]['Id'],
+                              'Name': domain
+                              }
+    return domain_ids
+
+
+def discover_ips() -> dict:
+    """Discover IPv4 address and IPv6 prefix"""
+    print(f"{datetime.now()} Starting IP discovery...")
+    if "ipv4" in conf["sources"]:
+        print("Trying to discover IPv4 address...")
+        ipv4_address = get_ipv4_address(conf["sources"]["ipv4"])
+        if ipv4_address:
+            print(f"Discovered IPv4 address: {ipv4_address}")
+    else:
+        ipv4_address = None
+    if "ipv6" in conf["sources"]:
+        print("Trying to discover IPv6 prefix...")
+        ipv6_prefix = get_ipv6_prefix(conf["sources"]["ipv6"])
+        if ipv6_prefix:
+            print(f"Discovered IPv6 prefix: {ipv6_prefix}")
+    else:
+        ipv6_prefix = None
+    return {'ipv4': ipv4_address,
+            'ipv6': ipv6_prefix
+            }
+
+
+def generate_desired_records() -> dict:
+    """Generate list of desired records grouped by domain"""
+    records = {}
+    # Create domain keys
+    for record in conf['dns_records']:
+        domain = get_base_domain(record["hostname"])
+        if domain not in records:
+            records[domain] = []
+    # Add record to dictionary
+    for record in conf['dns_records']:
+        if record["ipv4"] and 'ipv4' in current_ips:
+            domain = get_base_domain(record["hostname"])
+            records[domain].append({"Name": record["hostname"] + '.',
+                                    "ResourceRecords": [{"Value": str(current_ips['ipv4'])}],
+                                    "Type": "A",
+                                    "TTL": args.ttl
+                                    })
+        if record["ipv6"] and 'ipv6' in current_ips:
+            ipv6_address = calculate_ipv6_address(current_ips['ipv6'], record)
+            if ipv6_address:
+                domain = get_base_domain(record["hostname"])
+                records[domain].append({"Name": record["hostname"] + '.',
+                                        "ResourceRecords": [{"Value": str(ipv6_address)}],
+                                        "Type": "AAAA",
+                                        "TTL": args.ttl
+                                        })
+    return records
+
+
+def get_current_records() -> dict:
+    """Get the current values of the configured records"""
+    records = {}
+    for domain in domains:
+        records[domain] = get_route53_domain_records(domain_id=domains[domain]['Id'])
+    return records
+
+
+def print_record_change(modification_flag: str, record: dict):
+    print("%s %26s %6s %4s %s" % (modification_flag, record['Name'], record['TTL'],
+                                  record['Type'], record['ResourceRecords'][0]['Value']))
+
+
+def generate_changes() -> dict:
+    """Generate lists of changes grouped by domain"""
+    record_changes = {}
+    print("Generating change batch:")
+    for domain in domains:
+        record_changes[domain] = []
+        for desired_record in desired_records[domain]:
+            exists, updated = needs_update(
+                desired_record, current_records[domain])
+            logging.debug(
+                f"{desired_record['Type']} Record '{desired_record['Name']}' checked with results: exists={exists} updated={updated}")
+            if exists and updated:
+                print_record_change("  ", desired_record)
+            elif not exists:
+                print_record_change("++", desired_record)
+                change = create_route53_change('CREATE', desired_record)
+                record_changes[domain].append(change)
+            elif not updated:
+                current_record = find_record_in_list(current_records[domain], desired_record['Name'],
+                                                     desired_record['Type'])
+                print_record_change("--", current_record)
+                print_record_change("++", desired_record)
+                change = create_route53_change('UPSERT', desired_record)
+                record_changes[domain].append(change)
+    return record_changes
+
+
+def apply_changes():
+    """Submit changes to Route 53"""
+    for domain in domains:
+        if domain in changes and len(changes[domain]) > 0:
+            print(f"Submitting changes to domain {domain}...")
+            change_batch = create_route53_change_batch(changes[domain])
+            route53.change_resource_record_sets(HostedZoneId=domains[domain]['Id'], ChangeBatch=change_batch)
 
 
 parser = argparse.ArgumentParser(description='Route 53 DynDNS')
@@ -230,97 +346,11 @@ if args.aws_conf_file:
     os.environ['AWS_CONFIG_FILE'] = args.aws_conf_file
 route53 = boto3.client('route53')
 
-# Get all the domain IDs
-domains = []
-domain_ids = {}
-for record in conf["dns_records"]:
-    domain = get_base_domain(record['hostname'])
-    if domain not in domains:
-        domains.append(domain)
-for domain in domains:
-    hosted_zones = route53.list_hosted_zones_by_name(DNSName=domain)
-    domain_ids[domain] = hosted_zones['HostedZones'][0]['Id']
-
-# Discover IPs
-print(f"{datetime.now()} Starting IP discovery...")
-# Discover IPv4 and IPv6 addresses
-if "ipv4" in conf["sources"]:
-    print("Trying to discover IPv4 address...")
-    ipv4_address = get_ipv4_address(conf["sources"]["ipv4"])
-    if ipv4_address:
-        print(f"Discovered IPv4 address: {ipv4_address}")
-else:
-    ipv4_address = None
-if "ipv6" in conf["sources"]:
-    print("Trying to discover IPv6 prefix...")
-    ipv6_prefix = get_ipv6_prefix(conf["sources"]["ipv6"])
-    if ipv6_prefix:
-        print(f"Discovered IPv6 prefix: {ipv6_prefix}")
-else:
-    ipv6_prefix = None
-
-# Create desired state
-desired_records = {}
-for domain in domain_ids:
-    desired_records[domain] = []
-for record in conf["dns_records"]:
-    if record["ipv4"] and ipv4_address:
-        domain = get_base_domain(record["hostname"])
-        desired_records[domain].append({"name": record["hostname"] + '.',
-                                        "value": str(ipv4_address),
-                                        "type": "A",
-                                        "ttl": args.ttl
-                                        })
-    if record["ipv6"] and ipv6_prefix:
-        ipv6_address = calculate_ipv6_address(ipv6_prefix, record)
-        if ipv6_address:
-            domain = get_base_domain(record["hostname"])
-            desired_records[domain].append({"name": record["hostname"] + '.',
-                                            "value": str(ipv6_address),
-                                            "type": "AAAA",
-                                            "ttl": args.ttl
-                                            })
-# Get current state
-current_records = {}
-for domain in domain_ids:
-    current_records[domain] = get_route53_domain_records(domain_id=domain_ids[domain])
-
-# Prepare changes
-record_changes = {}
-for domain in domain_ids:
-    record_changes[domain] = []
-    for desired_record in desired_records[domain]:
-        exists, updated = needs_update(
-            desired_record, current_records[domain])
-        logging.debug(
-            f"{desired_record['type']} Record '{desired_record['name']}' checked with results: exists={exists} updated={updated}")
-        if exists and updated:
-            print("Entry OK:")
-            print("%26s %6s %4s %s" % (desired_record['name'], desired_record['ttl'],
-                                       desired_record['type'], desired_record['value']))
-        elif not exists:
-            print("Adding new entry:")
-            print("%26s %6s %4s %s" % (desired_record['name'], desired_record['ttl'],
-                                       desired_record['type'], desired_record['value']))
-            change = create_route53_change('CREATE', desired_record)
-            record_changes[domain].append(change)
-        elif not updated:
-            current_record = find_record_in_list(current_records[domain], desired_record['name'],
-                                                 desired_record['type'])
-            print("Modifying existing entry:")
-            print("OLD: %21s %6s %4s %s" % (current_record['Name'], current_record['TTL'],
-                                              current_record['Type'], current_record['ResourceRecords'][0]['Value']))
-            print("NEW: %21s %6s %4s %s" % (desired_record['name'], desired_record['ttl'],
-                                              desired_record['type'], desired_record['value']))
-            change = create_route53_change('UPSERT', desired_record)
-            record_changes[domain].append(change)
-
-# Apply changes
-for domain in domain_ids:
-    if record_changes[domain]:
-        print(f"Applying changes to domain {domain} ...")
-        change_batch = create_route53_change_batch(record_changes[domain])
-        result = route53.change_resource_record_sets(
-            HostedZoneId=domain_ids[domain], ChangeBatch=change_batch)
+domains = get_all_base_domains()
+current_ips = discover_ips()
+desired_records = generate_desired_records()
+current_records = get_current_records()
+changes = generate_changes()
+apply_changes()
 
 print("Goodbye.")
